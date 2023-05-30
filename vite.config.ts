416fef20393d7cb9406d3fbce0271bdf8b2594bf
lin-yen <lin-yen@mail.chungyo.net>
2023-05-30 15:45:06 +0800

    chore: 環境設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import postCssPxToRem from 'postcss-pxtorem'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [
+        postCssPxToRem({
+          rootValue: 16,
+          propList: ['*'],
+        }),
+        autoprefixer({
+          overrideBrowserslist: [
+            'last 2 versions',
+          ],
+          grid: true
+        })
+      ]
+    },
+  }
 })
