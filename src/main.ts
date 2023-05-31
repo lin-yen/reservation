0bdbe1924bd4d9165aa84d3cf174df646eecc944
lin-yen <lin-yen@mail.chungyo.net>
2023-05-31 16:52:05 +0800

    feat(pkg): use bootstrap-vue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 
-createApp(App).mount('#app');
+createApp(App)
+  .use(BootstrapVue as any)
+  .use(BootstrapVueIcons as any)
+  .mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      vue: '@vue/compat',
+    },
+  },
   plugins: [vue(), eslintPlugin()],
   css: {
     postcss: {
