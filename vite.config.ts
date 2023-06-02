a2c8cc178c87e13e26d6772e9cf3fdce37e15e5b
lin-yen <lin-yen@mail.chungyo.net>
2023-06-02 16:47:48 +0800

    feat(pkg): use unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import autoprefixer from 'autoprefixer';
 import eslintPlugin from 'vite-plugin-eslint';
 import legacy from '@vitejs/plugin-legacy';
 import * as path from 'path';
+import Components from 'unplugin-vue-components/vite';
+import { BootstrapVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,10 +17,22 @@ export default defineConfig({
     },
   },
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          compatConfig: {
+            MODE: 2,
+          },
+        },
+      },
+    }),
     eslintPlugin(),
     legacy({
       targets: ['defaults', 'not IE 11'],
+    }),
+    Components({
+      resolvers: [BootstrapVueResolver()],
+      dts: true, // enabled by default if `typescript` is installed
     }),
   ],
   css: {
